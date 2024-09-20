7d547ce64b4d5b41f9868a31845d780c165eb22c
juliengrima <gogrinformatique@gmail.com>
2024-09-20 11:01:04 +0200

    add all Websites

--- src/EventSubscriber/TwigEventSubscriber.php
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -4,6 +4,7 @@
 namespace App\EventSubscriber;
 
 use App\Repository\GamesRepository;
+use App\Repository\WebsitesRepository;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -13,17 +14,23 @@ class TwigEventSubscriber implements EventSubscriberInterface
 {
     private $twig;
     private $gamesRepository;
+    private $websitesRepository;
 
-    public function __construct(Environment $twig, GamesRepository $gamesRepository)
+    public function __construct(Environment $twig, 
+                                GamesRepository $gamesRepository, 
+                                WebsitesRepository $websitesRepository)
     {
         $this->twig = $twig;
         $this->gamesRepository = $gamesRepository;
+        $this->websitesRepository = $websitesRepository;
     }
 
     public function onKernelController(ControllerEvent $event)
     {
         $games = $this->gamesRepository->findAll();
+        $websites = $this->websitesRepository->findAll();
         $this->twig->addGlobal('games', $games);
+        $this->twig->addGlobal('websites', $websites);
     }
 
     public static function getSubscribedEvents()
